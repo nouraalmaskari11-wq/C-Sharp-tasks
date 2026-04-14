aa50ddbbad8e958efba5e71db6657d61891a3747
“Noura” <noura.almaskari11@gmail.com>
2026-04-14 15:33:23 +0400

    adding Day-01 task

---
+++ Solution01/Project01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please Enter First Number !");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Please Enter second Number !");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{num1}+ {num2}= {num1 + num2}");
+            Console.WriteLine($"{num1}- {num2}= {num1 - num2}");
+            Console.WriteLine($"{num1}* {num2}= {num1 * num2}");
+            Console.WriteLine($"{num1}/ {num2}= {num1 / num2}");
+
+        }
+    }
+}
