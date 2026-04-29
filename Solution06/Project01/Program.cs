2d58438673e0515a3c26fa06944e27980d9d7c65
“Noura” <noura.almaskari11@gmail.com>
2026-04-29 09:04:08 +0400

    for / foreach

---
+++ Solution06/Project01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Name = new string [5];
+
+            for (int i=0; i < Name.Length; i++)
+            {
+                Console.WriteLine("Enter your Name");
+                Name[i]= Convert.ToString(Console.ReadLine());
+            }
+
+
+            Console.WriteLine(" Name entered are >>>");
+
+                foreach (string cup in Name)
+            {
+                Console.WriteLine(cup);
+            }
+
+
+
+
+
+
+        }
+    }
+}
